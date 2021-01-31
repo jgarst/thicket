1eda0ee6e59d6dff045dd8ba56fe98eb30e4e610
Jared Hall Garst <jhg@fastmail.com>
2021-01-31 15:37:19 -0500

    project has a minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sphinxcontrib-thicket',
+    description='interlinked notes for Sphinx',
+    version=0.0,
+    author='Jared Garst',
+    author_email='jhg@fastmail.com',
+    install_requires=['sphinx'],
+    namespace_packages=['sphinxcontrib'],
+    packages=['sphinxcontrib'],
+)
